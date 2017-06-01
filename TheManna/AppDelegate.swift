eb949b43b0b567f17403d5f3f045e276336fe6d8
Sean Zhang <sean7218@gmail.com>
2017-06-01 13:08:55 -0400

    Add tableViewController

--- TheManna/AppDelegate.swift
+++ TheManna/AppDelegate.swift
@@ -13,10 +13,16 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var signInViewController: UIViewController?
 
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = UINavigationController(rootViewController: RootViewController())
+        
         return true
     }
 
---
+++ TheManna/RootViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RootViewController.swift
+//  TheManna
+//
+//  Created by Sean Zhang on 6/1/17.
+//  Copyright © 2017 Sean Zhang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RootViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
