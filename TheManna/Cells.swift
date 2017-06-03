bbb15bcdcaee1f030c48a08224d959753ba27c44
Sean Zhang <sean7218@gmail.com>
2017-06-02 20:17:10 -0400

    Adding button and also the imageProfileView

--- TheManna/Cells.swift
+++ TheManna/Cells.swift
@@ -28,40 +28,64 @@ class UserCell: DatasourceCell {
     
     let profileImageView: UIImageView = {
         let imageView = UIImageView()
-        imageView.backgroundColor = .red
+        imageView.image = #imageLiteral(resourceName: "selfie")
+        imageView.layer.cornerRadius = 5
+        imageView.layer.masksToBounds = true
+        imageView.contentMode = .scaleAspectFill
         return imageView
     }()
     
     override var datasourceItem: Any? {
         didSet {
-            nameLabel.text = datasourceItem as? String
+            //nameLabel.text = datasourceItem as? String
         }
     }
     
     let nameLabel: UILabel = {
         let label = UILabel()
-        label.text = "TEST TEST"
-        label.backgroundColor = UIColor.brown
+        label.text = "Sean Zhang"
+   
+        label.font = UIFont.boldSystemFont(ofSize: 16)
         return label
     }()
     
     let usernameLabel: UILabel = {
         let label = UILabel()
-        label.text = "NAME NAME NAME"
-        label.backgroundColor = UIColor.purple
+        label.text = "@sean7218"
+        label.textColor = UIColor(r: 130, g: 130, b: 130)
+        label.font = UIFont.systemFont(ofSize: 15)
         return label
     }()
     
     let bioTextView: UITextView = {
         let textView = UITextView()
         textView.text = "THis is Sean Zhang"
-        textView.backgroundColor = UIColor.darkGray
+        textView.text = "iPhone iPad iOS programming community and join us to learn Swift or Objective-C to build apps that we can do a lot of fun things"
+        textView.font = UIFont.systemFont(ofSize: 15)
         return textView
     }()
     
     let followButton: UIButton = {
+        let twitterBlue = UIColor(r: 61, g: 167, b: 244)
         let button = UIButton()
-        button.backgroundColor = UIColor.cyan
+        button.layer.cornerRadius = 5
+        button.layer.borderColor = twitterBlue.cgColor
+        button.layer.borderWidth = 1
+        button.setTitle("Follow", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        button.setTitleColor(twitterBlue, for: .normal)
+        button.setImage(#imageLiteral(resourceName: "follow"), for: .normal)
+        button.imageView?.contentMode = .scaleAspectFit
+        
+        button.titleLabel?.translatesAutoresizingMaskIntoConstraints = false
+        button.titleLabel?.centerXAnchor.constraint(equalTo: button.centerXAnchor, constant: 5).isActive = true
+        
+        button.imageView?.translatesAutoresizingMaskIntoConstraints = false
+        button.imageView?.widthAnchor.constraint(equalToConstant: 20).isActive = true
+        button.imageView?.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        button.imageView?.rightAnchor.constraint(equalTo: (button.titleLabel?.leftAnchor)!, constant: -5).isActive = true
+        
+        
         return button
     }()
     
@@ -78,9 +102,9 @@ class UserCell: DatasourceCell {
         
         nameLabel.anchor(profileImageView.topAnchor, left: profileImageView.rightAnchor, bottom: nil, right: followButton.leftAnchor, topConstant: 0, leftConstant: 8, bottomConstant: 0, rightConstant: 12, widthConstant: 0, heightConstant: 20)
         
-        usernameLabel.anchor(nameLabel.bottomAnchor, left: profileImageView.rightAnchor, bottom: nil, right: followButton.leftAnchor, topConstant: 4, leftConstant: 8, bottomConstant: 0, rightConstant: 12, widthConstant: 0, heightConstant: 20)
+        usernameLabel.anchor(nameLabel.bottomAnchor, left: profileImageView.rightAnchor, bottom: nil, right: followButton.leftAnchor, topConstant: 0, leftConstant: 8, bottomConstant: 0, rightConstant: 12, widthConstant: 0, heightConstant: 20)
         
-        bioTextView.anchor(usernameLabel.bottomAnchor, left: usernameLabel.leftAnchor, bottom: self.bottomAnchor, right: self.rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+        bioTextView.anchor(usernameLabel.bottomAnchor, left: usernameLabel.leftAnchor, bottom: self.bottomAnchor, right: self.rightAnchor, topConstant: 4, leftConstant: -4, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
         
         followButton.anchor(self.topAnchor, left: nil, bottom: nil, right: self.rightAnchor, topConstant: 12, leftConstant: 0, bottomConstant: 0, rightConstant: 12, widthConstant: 120, heightConstant: 34)
     }
